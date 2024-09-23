59e59c62fe570cf07b140a7ce764d21dc806c974
Orhan Güzel <orhan.guezel@dci-student.org>
2024-09-23 13:10:15 +0200

    alles gut

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,20 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const navMenu = document.querySelector('.nav-menu');
 
     hamburgerMenu.addEventListener('click', function () {
-        // Menüdeki 'show' sınıfı açıp kapatıyoruz
+ 
         navMenu.classList.toggle('show');
         
-        // Hamburger menüsünü X şekline dönüştürmek için 'open' sınıfını ekliyoruz
+     
         hamburgerMenu.classList.toggle('open');
     });
 });
 
 
-// Ortadaki resme tıklayınca animasyonu tetikleyin
-document.querySelector('.logo-about-me img').addEventListener('click', function() {
-    document.querySelector('.logo-sport').classList.add('move-to-position');
-    document.querySelector('.logo-education').classList.add('move-to-position');
-    document.querySelector('.logo-sosial').classList.add('move-to-position');
-    document.querySelector('.logo-projects').classList.add('move-to-position');
-  });
-  
+
+ 
