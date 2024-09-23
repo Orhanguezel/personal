169e5714b0148d27c08c619e047e5ae329f48733
DCI Student <student@example.com>
2024-09-23 13:31:00 +0200

    Updates

--- js/loader.js
+++ js/loader.js
@@ -1,22 +1,32 @@
-// loader.js
-
 const body = document.querySelector("body"),
-    loader = document.querySelector(".loader-wrap");
+    loader = document.querySelector(".progress-wrap");
 
-// Remove loader after page load
-function fadeOutEffect() {
-    const fadeEffect = setInterval(function() {
-        if (!loader.style.opacity) {
-            loader.style.opacity = 1;
-        }
-        if (loader.style.opacity > 0) {
-            loader.style.opacity -= 0.4;
-        } else {
-            body.classList.remove('stop-scroll');
-            loader.classList.add('remove');
-            clearInterval(fadeEffect);
-        }
-    }, 100);
+
+if (!localStorage.getItem('hasLoaded')) {
+    // Loader'ı göster
+    function fadeOutEffect() {
+        const fadeEffect = setInterval(function() {
+            if (!loader.style.opacity) {
+                loader.style.opacity = 1;
+            }
+            if (loader.style.opacity > 0) {
+                loader.style.opacity -= 0.1;
+            } else {
+                body.classList.remove('stop-scroll');
+                loader.classList.add('remove');
+                clearInterval(fadeEffect);
+            }
+        }, 100);
+    }
+
+    window.addEventListener("load", function() {
+        setTimeout(fadeOutEffect, 2000); 
+    });
+
+
+    localStorage.setItem('hasLoaded', true);
+} else {
+
+    loader.style.display = "none";
 }
 
-window.addEventListener("load", fadeOutEffect);
