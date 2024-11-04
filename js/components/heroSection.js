00e8e8c06b699dde9ca48201b1b57ae984d00395
Orhan Guezel <orhan.guezel@dci-student.org>
2024-11-04 22:39:07 +0100

    navbar und hero sind fertig

--- js/components/heroSection.js
+++ js/components/heroSection.js
@@ -3,7 +3,7 @@ export function loadHeroSection() {
     heroSection.innerHTML = `
       <section class="hero" id="hero">
         <div class="hero-content">
-          <h2>Transforming Ideas into <span class="highlight">Digital Experiences</span></h2>
+          <h2>Transforming Ideas into <span class="highlight-text">Digital Experiences</span></h2>
           <p>
             I am a web developer specializing in creating unique, high-performance websites and web applications.
             With a focus on user experience and modern design, I bring ideas to life on the web.
