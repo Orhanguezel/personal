f59ea6a2f3b5b54a274edf0904531780443d7a28
Orhan Guezel <orhan.guezel@dci-student.org>
2024-11-05 00:07:10 +0100

    skill ist fertig

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ import { loadHeader } from './components/header.js';
 import { loadHeroSection } from './components/heroSection.js';
 import { loadAboutMeSection } from './components/aboutMeSection.js';
 import { loadProjectsSection } from './components/portfolyoSection.js';
+import { loadSkillsSection } from './components/skillSection.js';
 import { loadFooter } from './components/footer.js';
 
 
@@ -10,4 +11,5 @@ loadHeader();
 loadHeroSection();
 loadAboutMeSection();
 loadProjectsSection();
+loadSkillsSection();
 loadFooter();
