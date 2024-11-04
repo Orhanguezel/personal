a86d83c745efea1e96e717cf984e9f7a3e553850
Orhan Guezel <orhan.guezel@dci-student.org>
2024-11-04 23:41:49 +0100

    portfolio ist ok

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 import { loadHeader } from './components/header.js';
 import { loadHeroSection } from './components/heroSection.js';
 import { loadAboutMeSection } from './components/aboutMeSection.js';
-import { loadProjectsSection } from './components/projectsSection.js';
+import { loadProjectsSection } from './components/portfolyoSection.js';
 import { loadFooter } from './components/footer.js';
 
 
