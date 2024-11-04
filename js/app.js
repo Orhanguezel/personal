25b98fd204a491af089f7c1ebdcd0ebaf7dc706b
Orhan Guezel <orhan.guezel@dci-student.org>
2024-11-04 21:58:06 +0100

    hero ist fertig

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
-import { loadLoader } from './components/loader.js';
+
 import { loadHeader } from './components/header.js';
-import { loadNavbar } from './components/navbar.js';
 import { loadHeroSection } from './components/heroSection.js';
 import { loadAboutMeSection } from './components/aboutMeSection.js';
 import { loadProjectsSection } from './components/projectsSection.js';
 import { loadFooter } from './components/footer.js';
 
-loadLoader();
+
 loadHeader();
-loadNavbar();
 loadHeroSection();
 loadAboutMeSection();
 loadProjectsSection();
