1011cb8b983cf877d12d604f168107d84765bf14
Orhan Guezel <orhan.guezel@dci-student.org>
2024-11-05 01:29:20 +0100

    contact ist fertig

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
-
 import { loadHeader } from './components/header.js';
 import { loadHeroSection } from './components/heroSection.js';
 import { loadAboutMeSection } from './components/aboutMeSection.js';
 import { loadProjectsSection } from './components/portfolyoSection.js';
 import { loadSkillsSection } from './components/skillSection.js';
+import { loadContactSection } from './components/contactSection.js';
 import { loadFooter } from './components/footer.js';
+import { loadContactPage } from './components/contactPage.js';
 
+const currentPage = window.location.pathname;
 
+// Tüm sayfalarda geçerli olan bileşenleri yükle
 loadHeader();
-loadHeroSection();
-loadAboutMeSection();
-loadProjectsSection();
-loadSkillsSection();
 loadFooter();
+
+// Ana sayfa (index.html) için bileşenleri yükle
+if (currentPage.includes('index.html') || currentPage === '/') {
+  loadHeroSection();
+  loadAboutMeSection();
+  loadProjectsSection();
+  loadSkillsSection();
+  loadContactSection(); // Yalnızca `index.html` için kısa iletişim bölümü yüklenecek
+}
+
+// `contact.html` sayfasında yalnızca tam iletişim sayfası yüklenir
+if (currentPage.includes('contact.html')) {
+  loadContactPage(); // Tam iletişim sayfası sadece `contact.html`de yüklenecek
+}
