bce1d07937552f0963dab40b256c0ae864e2b4f7
Orhan Guezel <orhan.guezel@dci-student.org>
2024-12-10 19:54:04 +0100

    degisiklik

--- js/components/skillSection.js
+++ js/components/skillSection.js
@@ -12,6 +12,7 @@ export function loadSkillsSection() {
           <div class="icon-card"><img src="assets/css.png" alt="CSS" class="skill-icon" /><p>CSS</p></div>
           <div class="icon-card"><img src="assets/sass.png" alt="Sass" class="skill-icon" /><p>Sass</p></div>
           <div class="icon-card"><img src="assets/bootstrap.png" alt="Bootstrap" class="skill-icon" /><p>Bootstrap</p></div>
+          <div class="icon-card"><img src="assets/tailwindcss.png" alt="Tailwind CSS" class="skill-icon" /><p>Tailwind CSS</p></div>
           <div class="icon-card"><img src="assets/javascript.png" alt="JavaScript" class="skill-icon" /><p>JavaScript</p></div>
           <div class="icon-card"><img src="assets/react.png" alt="React.js" class="skill-icon" /><p>React.js</p></div>
           <div class="icon-card"><img src="assets/typescript.png" alt="TypeScript" class="skill-icon" /><p>TypeScript</p></div>
@@ -20,12 +21,12 @@ export function loadSkillsSection() {
         <!-- Backend Development -->
         <div class="skill-item">
           <h3>Backend Development</h3>
-          <div class="icon-card"><img src="assets/nodejs.png" alt="Node.js" class="skill-icon" /><p>NodeJS</p></div>
+          <div class="icon-card"><img src="assets/nodejs.png" alt="Node.js" class="skill-icon" /><p>Node.js</p></div>
           <div class="icon-card"><img src="assets/expressjs.png" alt="Express.js" class="skill-icon" /><p>Express.js</p></div>
           <div class="icon-card"><img src="assets/mongodb.png" alt="MongoDB" class="skill-icon" /><p>MongoDB</p></div>
           <div class="icon-card"><img src="assets/mysql.png" alt="MySQL" class="skill-icon" /><p>MySQL</p></div>
-          <div class="icon-card"><img src="assets/postman.svg" alt="Postman" class="skill-icon" /><p>Postman</p></div>
           <div class="icon-card"><img src="assets/jest.png" alt="Jest" class="skill-icon" /><p>Jest</p></div>
+          <div class="icon-card"><img src="assets/postman.svg" alt="Postman" class="skill-icon" /><p>Postman</p></div>
         </div>
         
         <!-- UI/UX Design -->
@@ -36,12 +37,19 @@ export function loadSkillsSection() {
           <div class="icon-card"><img src="assets/photoshop.png" alt="Adobe Photoshop" class="skill-icon" /><p>Adobe Photoshop</p></div>
         </div>
         
+        <!-- DevOps and Tools -->
+        <div class="skill-item">
+          <h3>DevOps and Tools</h3>
+          <div class="icon-card"><img src="assets/git.png" alt="Git" class="skill-icon" /><p>Git</p></div>
+          <div class="icon-card"><img src="assets/GitHub.jpg" alt="GitHub" class="skill-icon" /><p>GitHub</p></div>
+          <div class="icon-card"><img src="assets/docker.png" alt="Docker" class="skill-icon" /><p>Docker</p></div>
+          <div class="icon-card"><img src="assets/vercel.png" alt="Vercel" class="skill-icon" /><p>Vercel</p></div>
+        </div>
+        
         <!-- Other Skills -->
         <div class="skill-item">
           <h3>Other Skills</h3>
           <div class="icon-card"><img src="assets/seo.jpg" alt="SEO" class="skill-icon" /><p>SEO</p></div>
-          <div class="icon-card"><img src="assets/git.png" alt="Git" class="skill-icon" /><p>Git</p></div>
-          <div class="icon-card"><img src="assets/GitHub.jpg" alt="GitHub" class="skill-icon" /><p>GitHub</p></div>
           <div class="icon-card"><img src="assets/wordpress.jpg" alt="WordPress" class="skill-icon" /><p>WordPress</p></div>
         </div>
         
